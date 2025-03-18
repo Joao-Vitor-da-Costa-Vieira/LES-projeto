be83bdae0ace8f661011b7be1d3b2f1aba0b02b3
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-03-18 10:34:29 -0300

    Alteração e ajustes na formatação das views

--- Front End/public/scripts/mensagemEndEntrega.js
+++ Front End/public/scripts/mensagemEndEntrega.js
@@ -25,15 +25,12 @@ document.querySelectorAll('.endereco-mostrado .endereco').forEach(endereco => {
                 </div>
                 <h2>Dados de ${enderecoDetalhes.end_endereco}</h2>
                 <p><strong>Nome: </strong>${enderecoDetalhes.end_nome}<p/>
-                <p><strong>Tipo de Residência: </strong>${enderecoDetalhes.end_tipoResidencia}<p/>
-                <p><strong>Tipo de Endereço: </strong>${enderecoDetalhes.end_tipoEndereco}<p/>
                 <p><strong>Endereço: </strong>${enderecoDetalhes.end_endereco}<p/>
                 <p><strong>Número: </strong>${enderecoDetalhes.end_numero}<p/>
                 <p><strong>Bairro: </strong>${enderecoDetalhes.end_bairro}<p/>
                 <p><strong>CEP: </strong>${enderecoDetalhes.end_cep}<p/>
                 <p><strong>Cidade: </strong>${enderecoDetalhes.end_cidade}<p/>
                 <p><strong>Estado: </strong>${enderecoDetalhes.end_estado}<p/>
-                <p><strong>País: </strong>${enderecoDetalhes.end_pais}<p/>
             `;
 
             containerEndereco.appendChild(mensagem);
