14ff7857dd39b7b4d8aff84abfd73226984681d2
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-03-31 13:58:51 -0300

    Adição de Função ao Home da Navbar

--- root/Front End/public/scripts/home.js
+++ root/Front End/public/scripts/home.js
@@ -1,3 +1,5 @@
+import {getHome} from "/scripts/service/telaInicialService.js";
+
 document.addEventListener('DOMContentLoaded', function() {
     const slide = document.querySelector('.carrosel-slide');
     const slides = document.querySelectorAll('.slide-item');
@@ -56,4 +58,12 @@ document.addEventListener('DOMContentLoaded', function() {
         slide.style.transition = "none";
         slide.style.transform = 'translateX(' + (-slides[0].clientWidth * counter) + 'px)';
     });
+
+    document.querySelectorAll('.home-link').forEach(botao => {
+            botao.addEventListener('click', (event) => {
+                event.stopPropagation();
+                console.log("Botão clicado!");
+                getHome('1');
+            });
+        });
 });
