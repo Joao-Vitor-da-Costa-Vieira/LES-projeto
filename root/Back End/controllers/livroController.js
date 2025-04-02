1b89fb19195bee79f6a4e6ced4fa9ae6cc1aebe3
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-04-02 15:33:25 -0300

    Adição de protótipo da view de pesquisarLivro

--- root/Back End/controllers/livroController.js
+++ root/Back End/controllers/livroController.js
@@ -12,12 +12,12 @@ module.exports.pesquisarLivrosTitulo = async (req, res) => {
         
         const livros = await buscarLivrosTitulo(titulo);
         const usuario = await buscarUsuarioId(usr_id);
-
-        if (req.xhr || req.headers.accept.indexOf('json') > -1) {
-            return res.json({ livros, usuario });
-        }
         
-        res.render('pesquisarLivro', { livros, usuario });
+        res.render('pesquisarLivro', { 
+            livros, 
+            usuario 
+        });
+        
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Erro ao pesquisar livros' });
