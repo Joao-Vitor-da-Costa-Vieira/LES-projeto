0620481eb8a8a4f985f74a7d69a7a04ece287cb1
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-04-19 17:56:33 -0300

    Adaptação do bancoDDL para o Xampp

--- bancoDDL.sql
+++ bancoDDL.sql
@@ -1,6 +1,7 @@
+use projetoles;
 CREATE TABLE autor (
     atr_id   INTEGER NOT NULL,
-    atr_nome VARCHAR2(75) NOT NULL
+    atr_nome VARCHAR(75) NOT NULL
 );
 
 ALTER TABLE autor ADD CONSTRAINT autor_pk PRIMARY KEY ( atr_id );
@@ -17,39 +18,24 @@ ALTER TABLE carrinho ADD CONSTRAINT carrinho_pk PRIMARY KEY ( car_id );
 CREATE TABLE cartoes (
     crt_id               INTEGER NOT NULL AUTO_INCREMENT,
     crt_numero           INTEGER NOT NULL,
-    crt_bandeira         VARCHAR2(20) NOT NULL,
+    crt_bandeira         VARCHAR(20) NOT NULL,
     crt_codigo_seguranca INTEGER NOT NULL,
-    crt_nome             VARCHAR2(50) NOT NULL,
+    crt_nome             VARCHAR(50) NOT NULL,
     usuarios_usr_id       INTEGER NOT NULL
 );
-
-COMMENT ON COLUMN cartoes.crt_id IS
-    'Código de Identificação do Cartão';
-
-COMMENT ON COLUMN cartoes.crt_numero IS
-    'Numero do Cartão';
-
-COMMENT ON COLUMN cartoes.crt_bandeira IS
-    'Bandeira do Cartão';
-
-COMMENT ON COLUMN cartoes.crt_codigo_seguranca IS
-    'Código de Segurança do Cartão';
-
-COMMENT ON COLUMN cartoes.crt_nome IS
-    'Nome do Titular do Cartão';
 
 ALTER TABLE cartoes ADD CONSTRAINT cartao_pk PRIMARY KEY ( crt_id );
 
 CREATE TABLE categoria (
     cat_id   INTEGER NOT NULL,
-    cat_nome VARCHAR2(25) NOT NULL
+    cat_nome VARCHAR(25) NOT NULL
 );
 
 ALTER TABLE categoria ADD CONSTRAINT categoria_pk PRIMARY KEY ( cat_id );
 
 CREATE TABLE editora (
     edi_id   INTEGER NOT NULL,
-    edi_nome VARCHAR2(50) NOT NULL
+    edi_nome VARCHAR(50) NOT NULL
 );
 
 ALTER TABLE editora ADD CONSTRAINT editora_pk PRIMARY KEY ( edi_id );
@@ -64,71 +50,29 @@ ALTER TABLE editou ADD CONSTRAINT relation_14_pk PRIMARY KEY ( editora_edi_id,
 
 CREATE TABLE enderecos_cobranca (
     end_id          INTEGER NOT NULL AUTO_INCREMENT,
-    end_estado      VARCHAR2(2) NOT NULL,
-    end_cidade      VARCHAR2(50) NOT NULL,
-    end_bairro      VARCHAR2(25) NOT NULL,
+    end_estado      VARCHAR(2) NOT NULL,
+    end_cidade      VARCHAR(50) NOT NULL,
+    end_bairro      VARCHAR(25) NOT NULL,
     end_numero      INTEGER NOT NULL,
-    end_endereco    VARCHAR2(100) NOT NULL,
-    end_complemento VARCHAR2(20),
+    end_endereco    VARCHAR(100) NOT NULL,
+    end_complemento VARCHAR(20),
     end_cep         INTEGER NOT NULL,
     usuarios_usr_id  INTEGER NOT NULL
 );
-
-COMMENT ON COLUMN enderecos_cobranca.end_id IS
-    'Código de Identificação do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_estado IS
-    'Estado do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_cidade IS
-    'Cidade do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_bairro IS
-    'Bairro do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_numero IS
-    'Numero do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_complemento IS
-    'Complemento do Endereço de Cobrança';
-
-COMMENT ON COLUMN enderecos_cobranca.end_cep IS
-    'CEP do  Endereço de Cobrança';
 
 ALTER TABLE enderecos_cobranca ADD CONSTRAINT endereço_cobrança_pk PRIMARY KEY ( end_id );
 
 CREATE TABLE enderecos_entrega (
     end_id          INTEGER NOT NULL AUTO_INCREMENT,
-    end_estado      VARCHAR2(2) NOT NULL,
-    end_cidade      VARCHAR2(50) NOT NULL,
-    end_bairro      VARCHAR2(25) NOT NULL,
+    end_estado      VARCHAR(2) NOT NULL,
+    end_cidade      VARCHAR(50) NOT NULL,
+    end_bairro      VARCHAR(25) NOT NULL,
     end_numero      INTEGER NOT NULL,
-    end_endereco    VARCHAR2(100) NOT NULL,
-    end_complemento VARCHAR2(20),
+    end_endereco    VARCHAR(100) NOT NULL,
+    end_complemento VARCHAR(20),
     end_cep         INTEGER NOT NULL,
     usuarios_usr_id  INTEGER NOT NULL
 );
-
-COMMENT ON COLUMN enderecos_entrega.end_id IS
-    'Código de Identificação do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_estado IS
-    'Estado do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_cidade IS
-    'Cidade do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_bairro IS
-    'Bairro do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_numero IS
-    'Numero do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_complemento IS
-    'Complemento do Endereço de Entrega';
-
-COMMENT ON COLUMN enderecos_entrega.end_cep IS
-    'CEP do  Endereço de Entrega';
 
 ALTER TABLE enderecos_entrega ADD CONSTRAINT endereço_entrega_pk PRIMARY KEY ( end_id );
 
@@ -140,10 +84,9 @@ CREATE TABLE escreveu (
 ALTER TABLE escreveu ADD CONSTRAINT relation_15_pk PRIMARY KEY ( autor_atr_id,
                                                                  livros_lvr_id );
 
-
 CREATE TABLE forma_de_pagamento (
     fpg_id             INTEGER NOT NULL AUTO_INCREMENT,
-    fpg_tipo           VARCHAR2(20) NOT NULL,
+    fpg_tipo           VARCHAR(20) NOT NULL,
     transacoes_tra_id  INTEGER NOT NULL,
     cartoes_crt_id     INTEGER
     );
@@ -152,8 +95,8 @@ ALTER TABLE forma_de_pagamento ADD CONSTRAINT forma_de_pagamento_pk PRIMARY KEY 
 
 CREATE TABLE grupo_de_precificacao (
     grp_id           INTEGER NOT NULL,
-    grp_nome         VARCHAR2(50) NOT NULL,
-    grp_margem_lucro NUMBER(4, 2) NOT NULL
+    grp_nome         VARCHAR(50) NOT NULL,
+    grp_margem_lucro DECIMAL(4, 2) NOT NULL
 );
 
 ALTER TABLE grupo_de_precificacao ADD CONSTRAINT grupo_de_precificacao_pk PRIMARY KEY ( grp_id );
@@ -170,22 +113,22 @@ ALTER TABLE itens_de_venda ADD CONSTRAINT itens_de_venda_pk PRIMARY KEY ( itv_id
 CREATE TABLE livros (
     lvr_id                       INTEGER NOT NULL,
     lvr_ano                      INTEGER NOT NULL,
-    lvr_titulo                   VARCHAR2(150) NOT NULL,
-    lvr_edicao                   VARCHAR2(30) NOT NULL,
+    lvr_titulo                   VARCHAR(150) NOT NULL,
+    lvr_edicao                   VARCHAR(30) NOT NULL,
     lvr_isbn                     INTEGER NOT NULL,
     lvr_numero_de_paginas        INTEGER NOT NULL,
-    lvr_sinopse                  VARCHAR2(500) NOT NULL,
-    lvr_altura                   NUMBER(5, 2) NOT NULL,
-    lvr_largura                  NUMBER(5, 2) NOT NULL,
+    lvr_sinopse                  VARCHAR(500) NOT NULL,
+    lvr_altura                   DECIMAL(5, 2) NOT NULL,
+    lvr_largura                  DECIMAL(5, 2) NOT NULL,
     lvr_peso                     INTEGER NOT NULL,
-    lvr_profundidade             NUMBER(5, 2) NOT NULL,
+    lvr_profundidade             DECIMAL(5, 2) NOT NULL,
     lvr_codigo_de_barras         INTEGER NOT NULL,
-    lvr_ponteiro_imagem          VARCHAR2(50) NOT NULL,
+    lvr_ponteiro_imagem          VARCHAR(50) NOT NULL,
     lvr_status                   INTEGER NOT NULL,
     lvr_qtd_estoque              INTEGER NOT NULL,
-    lvr_custo                    NUMBER(4, 2) NOT NULL,
+    lvr_custo                    DECIMAL(4, 2) NOT NULL,
     lvr_desconto                 INTEGER,
-    lvr_justificativa            VARCHAR2(200),
+    lvr_justificativa            VARCHAR(200),
     grupo_de_precificacao_grp_id INTEGER
 );
 
@@ -193,7 +136,7 @@ ALTER TABLE livros ADD CONSTRAINT livros_pk PRIMARY KEY ( lvr_id );
 
 CREATE TABLE notificacao (
     ntf_id INTEGER NOT NULL AUTO_INCREMENT,
-    ntf_mensagem VARCHAR2(150) NOT NULL,
+    ntf_mensagem VARCHAR(150) NOT NULL,
     usuarios_usr_id INTEGER NOT NULL
 );
 
@@ -209,11 +152,11 @@ ALTER TABLE possui4 ADD CONSTRAINT relation_16_pk PRIMARY KEY ( categoria_cat_id
 CREATE TABLE transacoes (
     tra_id                   INTEGER NOT NULL AUTO_INCREMENT,
     tra_data                 DATE NOT NULL,
-    tra_valor_frete          NUMBER(5, 2) NOT NULL,
-    tra_status               VARCHAR2(15) NOT NULL,
-    tra_valor                NUMBER(6, 2) NOT NULL,
-    tra_desconto             NUMBER(6, 2),
-    tra_subtotal             NUMBER(6, 2) NOT NULL,
+    tra_valor_frete          DECIMAL(5, 2) NOT NULL,
+    tra_status               VARCHAR(15) NOT NULL,
+    tra_valor                DECIMAL(6, 2) NOT NULL,
+    tra_desconto             DECIMAL(6, 2),
+    tra_subtotal             DECIMAL(6, 2) NOT NULL,
     enderecos_entrega_end_id INTEGER,
     usuarios_usr_id          INTEGER
 );
@@ -222,47 +165,17 @@ ALTER TABLE transacoes ADD CONSTRAINT transacoes_pk PRIMARY KEY ( tra_id );
 
 CREATE TABLE usuarios (
     usr_id                  INTEGER NOT NULL AUTO_INCREMENT,
-    usr_nome                VARCHAR2(75) NOT NULL,
-    usr_email               VARCHAR2(50) NOT NULL,
-    usr_cpf                 VARCHAR2(11) NOT NULL,
-    usr_senha               VARCHAR2(20) NOT NULL,
+    usr_nome                VARCHAR(75) NOT NULL,
+    usr_email               VARCHAR(50) NOT NULL,
+    usr_cpf                 VARCHAR(11) NOT NULL,
+    usr_senha               VARCHAR(20) NOT NULL,
     usr_data_de_nascimento  DATE NOT NULL,
     usr_telefone_1          INTEGER NOT NULL,
     usr_telefone_2          INTEGER,
-    usr_genero              VARCHAR2(10) NOT NULL,
+    usr_genero              VARCHAR(10) NOT NULL,
     usr_ranking             INTEGER,
     usr_status_de_atividade CHAR(1) NOT NULL
 );
-
-COMMENT ON COLUMN usuarios.usr_id IS
-    'Código de Identificação do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_nome IS
-    'Nome do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_email IS
-    'E-mail do Usúrario';
-
-COMMENT ON COLUMN usuarios.usr_cpf IS
-    'CPF do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_senha IS
-    'Senha do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_data_de_nascimento IS
-    'Data de Nascimento do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_telefone_1 IS
-    'Telefone do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_telefone_2 IS
-    'Telefone Alternativo do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_genero IS
-    'Genero do Usúario';
-
-COMMENT ON COLUMN usuarios.usr_status_de_atividade IS
-    'Status de Atividade do Usúario';
 
 ALTER TABLE usuarios ADD CONSTRAINT usuário_pk PRIMARY KEY ( usr_id );
 
@@ -329,3 +242,15 @@ ALTER TABLE possui4
 ALTER TABLE possui4
     ADD CONSTRAINT relation_16_livros_fk FOREIGN KEY ( livros_lvr_id )
         REFERENCES livros ( lvr_id );
+        
+ALTER TABLE notificacao
+    ADD CONSTRAINT fk_ntf_usr FOREIGN KEY ( usuarios_usr_id )
+        REFERENCES usuarios ( usr_id );
+        
+ALTER TABLE forma_de_pagamento
+    ADD CONSTRAINT fk_fpg_crt FOREIGN KEY ( cartoes_crt_id )
+        REFERENCES cartoes ( crt_id );
+
+ALTER TABLE forma_de_pagamento
+    ADD CONSTRAINT fk_fpg_tra FOREIGN KEY ( transacoes_tra_id )
+        REFERENCES transacoes ( tra_id );
