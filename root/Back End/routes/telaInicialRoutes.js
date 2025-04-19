635a21863d3cb2c29792450c25888cd082238b9e
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-04-19 18:48:27 -0300

    Adição de rota para home do ADM

--- root/Back End/routes/telaInicialRoutes.js
+++ root/Back End/routes/telaInicialRoutes.js
@@ -4,5 +4,6 @@ const telaInicialController = require('../controllers/telaInicialController');
 
 router.get('/telaInicial', telaInicialController.getTela);
 router.get('/home/:usr_id',telaInicialController.getHome);
+router.get('/home-adm',telaInicialController.getHomeAdm);
 
 module.exports = router;
