c17ef0c63002e8bbd1c9949b16548a886e0956ed
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-04-19 18:06:14 -0300

    Adição de função getHomeAdm ao telaInicialService

--- root/Front End/public/scripts/service/telaInicialService.js
+++ root/Front End/public/scripts/service/telaInicialService.js
@@ -9,3 +9,14 @@ export async function getHome(usr_id) {
         throw err;
     }
 } 
+
+export async function getHomeAdm() {
+    try {
+        window.location.href = `/home-adm`;
+        const data = await res.json(); 
+        return data;
+    } catch (err) {
+        console.error("Falha no fetch:", err);
+        throw err;
+    }
+} 
--- root/Front End/public/scripts/telaInicial.js
+++ root/Front End/public/scripts/telaInicial.js
@@ -1,4 +1,5 @@
 import {getHome} from "/scripts/service/telaInicialService.js";
+import {getHomeAdm} from "/scripts/service/telaInicialService.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.botao-usuario').forEach(botao => {
@@ -8,4 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
             getHome('1');
         });
     });
+
+    document.querySelectorAll('.botao-adm').forEach(botao => {
+        botao.addEventListener('click', (event) => {
+            event.stopPropagation();
+            console.log("Botão clicado!");
+            getHomeAdm;
+        });
+    });
 });
+
+
+
