489cc57c0b3c0674be547b2f5dbb71036de6672c
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-06-10 15:32:25 -0300

    Alteração da versão do Gemini usada

--- root/ai-service/config/chat.py
+++ root/ai-service/config/chat.py
@@ -25,7 +25,7 @@ if not api_key:
 client = genai.Client(api_key=api_key)
 
 #Defininfo o meumodelo e o conteúdo que eu vou mostrar
-model = 'gemini-2.0-flash'
+model = 'gemini-1.5-flash'
     
 #Função que reseta as intruções do chat
 def create_ai_chat():
