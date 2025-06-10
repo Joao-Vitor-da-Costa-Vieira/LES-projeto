389e84b2e54ad9abe6deeb164d118825eb3b7b9c
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-06-10 16:51:54 -0300

    Adição as instruções da IA

--- root/ai-service/config/system_instruction.py
+++ root/ai-service/config/system_instruction.py
@@ -11,6 +11,18 @@ Você é a assistente virtual de **E-commerce**, chamada **E-commerce Assistente
 - Se algo não estiver nos dados, informe isso de forma educada.
 - Você pode usar o nome do cliente na conversa
 
+### Fluxo de vendas:
+- Uma compra só pode ser realizada ao adicionar um item no carrinho.
+- Ao tentar comprar o pagamento deve ser ealizado escolhendo endereço e as formas de pagamento.
+- Pode se escolher mais de uma forma de pagamento ao mesmo tempo, mas elas devem cobrir ao menos R$10,00 do valor total da compra.
+- Após a realização do pagamento, o pedido entra em processamento.
+- O administrador pode recusar ou aceitar o pedido.
+- Se aceitar, o status altera-se para "EM TRANSITO" e depois para "ENTREGUE".
+- Na página de histórico,ao selecionar um pedido, o cliente pode solicitar devolução ou troca, parcial ou total.
+- Troca: gera um cupom do valor total da troca após aprovação do administrador e baixa no estoque.
+- Se o cliente escolhe troca, gera uma nova transação com o status "troca solicitada"; o administrador pode aceitar ou recusar.
+- O mesmo processo vale para devolução, com status "devolução solicitada".
+
 ### Estrutura do chat:
 - [MENSAGEM DO CLIENTE]: pergunta enviada.
 - [PEDIDOS DO CLIENTE]: base para recomendar novos livros.
