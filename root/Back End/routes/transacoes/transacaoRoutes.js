fbe0822c64241d8cb244eb2b0c59fb9cc5e524bf
João Vitor da Costa Vieira <jvcvieira2001@hotmail.com>
2025-06-19 14:38:51 -0300

    Ajuste nas routes do back End

--- root/Back End/routes/transacoes/transacaoRoutes.js
+++ root/Back End/routes/transacoes/transacaoRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const vendasController = require('../../controllers/transacoes/vendasController');
+const trocasController = require('../../controllers/transacoes/trocasController');
+const devolucoesController = require('../../controllers/transacoes/devolucoesController');
 
 //Views
 router.get('/pagamento',vendasController.getPagamento);
@@ -8,12 +10,12 @@ router.get('/pagamento/historico',vendasController.getHistorico);
 router.get('/pedidos', vendasController.getPedidos);
 router.get('/pedidos-adm/detalhes',vendasController.getPedidoItem);
 router.get('/pagamento/detalhes', vendasController.getTransacao);
-router.get('/trocas',vendasController.getTroca);
-router.get('/devolucoes',vendasController.getDevolucao);
+router.get('/trocas',trocasController.getTroca);
+router.get('/devolucoes',devolucoesController.getDevolucao);
 
 router.post('/pagamento/confirmar',vendasController.postPagamento);
-router.post('/trocas/confirmar',vendasController.postTroca);
-router.post('/devolucoes/confirmar',vendasController.postDevolucao);
+router.post('/trocas/confirmar',trocasController.postTroca);
+router.post('/devolucoes/confirmar',devolucoesController.postDevolucao);
 
 router.get('/api/pedidos/usuario/:usr_id', vendasController.getApiPedidosUsuario);
 router.get('/api/pedidos/filtrar', vendasController.filtrarPedidos);
